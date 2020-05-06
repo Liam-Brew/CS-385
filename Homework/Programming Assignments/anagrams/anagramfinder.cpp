a39189d7bc62141741020b4af64ec5453102485d
Liam Brew <lbrew@stevens.edu>
2020-05-06 15:05:31 -0400

    anagrams init

---
+++ Homework/Programming Assignments/anagrams/anagramfinder.cpp
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Name        : anagramfinder.cpp
+ * Author      : Liam Brew
+ * Version     : 1.0
+ * Date        : 05.06.2020
+ * Description : Scans a text file of words and finds the longest words that have one or more anagrams present.
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool load_words(const string &filename) {
+    //TODO
+    return false;
+}
+
+void clean_up() {
+    //TODO
+}
+
+int main(int argc, char *const argv[]) {
+    //TODO
+    return 1;
+}
